4a2cfefbb8b4a676b7658a3ba5a923fc974fd5a8
paiva <lcspaiva87@gmail.com>
2024-09-03 22:35:16 -0300

    feat: Update Dashboard to fetch and display multiple forms and their responses

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -39,15 +39,10 @@ function decryptData(data: string, secretKey: string): string {
 export default function Dashboard() {
   const id = "66d51d179e9d9324744971f0";
   const [responses, setResponses] = useState<FormResponse[]>([]);
-  const [form, setForm] = useState<Form[]>([]);
+  const [forms, setForms] = useState<Form[]>([]);
   const secretKey = 'sua-chave-secreta';
+  console.log(responses);
 
-  useEffect(() => {
-    if (id) {
-      fetchResponses();
-      fetchForm();
-    }
-  }, [id]);
 
   async function fetchResponses() {
     const response = await fetch(`/api/save-data-form`);
@@ -68,54 +63,82 @@ export default function Dashboard() {
     }
   }
 
-  async function fetchForm() {
+
+  async function fetchForms() {
     const response = await fetch(`/api/form/${id}`);
     if (response.ok) {
       const data = await response.json();
-      setForm(data);
+      setForms(data);
     }
   }
-
-  if (form.length === 0) {
+  useEffect(() => {
+    if (id) {
+      fetchResponses();
+      fetchForms();
+    }
+  }, [id]);
+  if (forms.length === 0) {
     return <div>Carregando...</div>;
   }
 
+
+
   return (
-    <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Respostas para: {form[0]?.title}</h1>
-      <Link href={`/forms/${id}`} className="text-blue-500 hover:underline mb-4 inline-block">
-        ← Voltar para o formulário
-      </Link>
-      <div className="overflow-x-auto">
-        <table className="min-w-full bg-white border border-gray-300">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="px-4 py-2 border-b">Data</th>
-              {form[0]?.fields.map((field) => (
-                <th key={field.id} className="px-4 py-2 border-b">
-                  {field.label}
-                </th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {responses.map((response) => (
-              <tr key={response.id} className="hover:bg-gray-50">
-                <td className="px-4 py-2 border-b">
-                  {new Date(response.createdAt).toLocaleString()}
-                </td>
-                {form[0]?.fields.map((field) => (
-                  <td key={`${response.id}-${field.id}`} className="px-4 py-2 border-b">
-                    {Array.isArray(response.data[field.id])
-                      ? (response.data[field.id] as string[]).join(', ')
-                      : response.data[field.id] as string}
-                  </td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    </div>
+    <main className="container mx-auto p-4">
+      {forms.map((form) => (
+        <div key={form.id}>
+          <header className="mb-8">
+            <h1 className="text-3xl font-bold mb-4">Respostas para: {form.title}</h1>
+            <nav>
+              <Link href={`/forms/${form.id}`} className="text-blue-500 hover:underline inline-block">
+                ← Voltar para o formulário
+              </Link>
+            </nav>
+          </header>
+
+          <section>
+            {responses.length > 0 && form.fields && form.fields.length > 0 ? (
+              <div className="overflow-x-auto bg-white shadow-md rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                        Data
+                      </th>
+                      {form.fields.map(({ label, id }) => (
+                        <th key={id} scope="col" className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                          {label}
+                        </th>
+                      ))}
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200">
+                    {responses.map((response) => (
+                      <tr key={response.id} className="hover:bg-gray-50">
+                        <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          {new Date(response.createdAt).toLocaleString()}
+                        </td>
+                        {form.fields.map((field) => (
+                          <td key={`${response.id}-${field.id}`} className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
+                            {response.data[field.id] !== undefined
+                              ? Array.isArray(response.data[field.id])
+                                ? (response.data[field.id] as string[]).join(', ')
+                                : response.data[field.id] as string
+                              : 'N/A'}
+                          </td>
+                        ))}
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+              </div>
+            ) : (
+              <p className="text-center text-gray-500 mt-4">Nenhuma resposta disponível.</p>
+            )}
+          </section>
+        </div>
+      ))}
+    </main>
   );
+
 }
